6015a3fe7433bbfefda74ae9f41f821de6cd9920
David_B <bachorsd@informatik.hu-berlin.de>
2018-02-06 01:13:58 +0100

    Added the workspace which already contains a display manager

---
+++ workspace/GameEngine/src/engineTester/MainGameLoop.java
@@ -0,0 +1,28 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+
+public class MainGameLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		DisplayManager.createDisplay();
+		
+		while(!Display.isCloseRequested()) {
+			
+			//game logic
+			//render
+			DisplayManager.updateDisplay();
+			
+		}
+		
+		DisplayManager.closeDisplay();
+
+	}
+
+}
